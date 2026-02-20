623f50c826a21c4af7b09c54c8cc82445775147f
Sheida Khodabakhshian <sheidakh@my.yorku.ca>
2026-02-19 19:51:06 -0500

    Initial commit for auctionSystem

---
+++ src/test/java/com/BidTech/auctionSystem/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BidTech.auctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
