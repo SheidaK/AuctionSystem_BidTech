b0129007be2e93a6f33c73a6384bf28983283dc2
vrindaashwar <vrinda03@my.yorku.ca>
2026-03-05 16:14:42 -0500

    first post test

--- src/main/java/com/BidTech/auctionSystem/IAMService/User.java
+++ src/main/java/com/BidTech/auctionSystem/IAMService/User.java
@@ -1,91 +1,138 @@
 package com.BidTech.auctionSystem.IAMService;
+
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-@Entity
-public class User {
-	private @Id @GeneratedValue Long id;
+import jakarta.persistence.Table;
 
-	//private int id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
 	private String first_name;
 	private String last_name;
 	private String userName;
 	private String email;
 	private String password;
-	 @OneToOne
-	   @JoinColumn(name = "address_id")
-	    private Address address;
 	private String role;
-	 public User() {
-	    }
-	public User(String first_name, String lastName, String userName, String email, String password,String role) {
+
+	@JsonIgnore
+	@OneToOne(optional = true)
+	@JoinColumn(name = "address_id", nullable = true)
+	private Address address;
+
+	// REQUIRED by JPA
+	public User() {}
+
+	// Constructor
+	public User(String first_name, String last_name, String userName,
+				String email, String password, String role) {
+
 		this.first_name = first_name;
-		this.last_name= lastName;
+		this.last_name = last_name;
 		this.userName = userName;
 		this.email = email;
 		this.password = password;
 		this.role = role;
 	}
-	// Getters
+
+	// ===== GETTERS =====
+
 	public Long getId() {
 		return id;
 	}
-	public String getRole() {
-		return role;
-	}
+
 	public String getFirstName() {
 		return first_name;
 	}
+
 	public String getLastName() {
 		return last_name;
 	}
-	public String getUsername() {
+
+	public String getUserName() {
 		return userName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
 	public Address getAddress() {
 		return address;
 	}
-	public boolean checkPassword(String enteredPassword) {
-		return enteredPassword==password;
-	}
-		// Setters
+
+	// ===== SETTERS =====
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public void setFirstName(String name) {
-		this.first_name = name;
+
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
 	}
-	public void setLastName(String name) {
-		this.last_name = name;
+
+	public void setLastName(String last_name) {
+		this.last_name = last_name;
 	}
-	public void setUsername(String username) {
-		this.userName = username;
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public void setAddress(String streetNumber, String streetName, String city, String postalCode, String country) {
-		Address newAddress = new Address(streetNumber,streetName,city, postalCode,country);
-		this.address = newAddress;
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
-	 @Override
-	  public int hashCode() {
-	    return Objects.hash(this.id, this.first_name, this.last_name, this.role);
-	  }
 
-	  @Override
-	  public String toString() {
-	    return "User{" + "id=" + this.id + ", firstName='" + this.first_name + '\'' + ", lastName='" + this.last_name
-	        + '\'' + ", role='" + this.role + '\'' +", email='" + this.email+ '\''+ '}';
-	  }
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	// password check
+	public boolean checkPassword(String enteredPassword) {
+		return password != null && password.equals(enteredPassword);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, first_name, last_name, role);
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", firstName='" + first_name + '\'' +
+				", lastName='" + last_name + '\'' +
+				", role='" + role + '\'' +
+				", email='" + email + '\'' +
+				'}';
+	}
 }
