4b0d610a44f655b6681bb659cdbbd03e75ec62d5
vrindaashwar <vrinda03@my.yorku.ca>
2026-03-06 21:24:42 -0500

    payment and catalogue integration fixes

--- src/main/java/com/BidTech/auctionSystem/config/PaymentDbConfig.java
+++ src/main/java/com/BidTech/auctionSystem/config/PaymentDbConfig.java
@@ -1,18 +1,21 @@
 package com.BidTech.auctionSystem.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.JpaTransactionManager;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
 import org.springframework.transaction.PlatformTransactionManager;
 
 import jakarta.persistence.EntityManagerFactory;
@@ -39,11 +42,17 @@ public class PaymentDbConfig {
 
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
-
         em.setPackagesToScan("com.BidTech.auctionSystem.payment.model");
 
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
+
+        // THIS PART IS WHAT CREATES THE TABLES
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.dialect", "org.hibernate.community.dialect.SQLiteDialect");
+
+        em.setJpaPropertyMap(properties);
 
         return em;
     }
