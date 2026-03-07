231905995fe253b95d66fa1351827c34593218f3
vrindaashwar <vrinda03@my.yorku.ca>
2026-03-06 19:32:01 -0500

    updated spring boot to inculde 3 services

--- src/main/java/com/BidTech/auctionSystem/AuctionSystemApplication.java
+++ src/main/java/com/BidTech/auctionSystem/AuctionSystemApplication.java
@@ -2,10 +2,12 @@ package com.BidTech.auctionSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"IAMService", "CatalogueService", "com.BidTech.auctionSystem"})
+@SpringBootApplication(scanBasePackages = {
+		"com.BidTech.auctionSystem",
+		"IAMService",
+		"CatalogueService"
+})
 public class AuctionSystemApplication {
 
 	public static void main(String[] args) {
