d99abcdbfb8d8c10df8b5ecec1d88cf48e936d14
wangzh64 <wangzh64@my.yorku.ca>
2026-03-29 21:19:36 -0400

    notifications.html and temporary RabbitMQ disable

--- src/main/java/com/BidTech/auctionSystem/RabbitMQConfig.java
+++ src/main/java/com/BidTech/auctionSystem/RabbitMQConfig.java
@@ -3,6 +3,7 @@ package com.BidTech.auctionSystem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.core.Queue; // <-- 1. Add this import
 
 @Configuration
 public class RabbitMQConfig {
@@ -10,5 +11,10 @@ public class RabbitMQConfig {
     @Bean
     public JacksonJsonMessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue("notification.queue", true);
     }
 }
